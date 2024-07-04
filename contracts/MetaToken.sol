27d80c2dc36a8c5b88005ae20b7cccd03ef8187f
parveezkhanw <parveezkhan563101@gmail.com>
2024-07-04 12:19:20 +0530

    Update MetaToken.sol

--- contracts/MetaToken.sol
+++ contracts/MetaToken.sol
@@ -1,17 +1,49 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "erc721a/contracts/ERC721A.sol";
 
-contract MetaToken is ERC20, Ownable {
-    constructor() ERC20("MetaToken", "MTA") {}
+contract CryptoTokens is ERC721A {
+    // The owner of the contract
+    address public owner;
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    // Maximum quantity of tokens that can be minted
+    uint256 public maxQuantity = 10;
+
+    // Base URL for the token metadata
+    string baseUrl = "generate few cryprotokens currencies.url";
+
+    // Prompt description for the tokens
+    string public prompt = "Collect Crypto Tokens from around the world";
+
+    // Constructor to set the initial values
+    constructor() ERC721A("CryptoTokens", "CTK") {
+        owner = msg.sender;
     }
 
-    function decimals() public pure override returns (uint8) {
-		return 0;
-	}
+    // Modifier to restrict certain functions to the owner only
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action!");
+        _;
+    }
+
+    // Function to mint new tokens, restricted to the owner
+    function mint(uint256 quantity) external payable onlyOwner {
+        require(
+            totalSupply() + quantity <= maxQuantity,
+            "You can not mint more than 10"
+        );
+        _mint(msg.sender, quantity);
+    }
+
+    // Internal function to return the base URL for token metadata
+    function _baseURI() internal view override returns (string memory) {
+        return baseUrl;
+    }
+
+    // External function to return the prompt description
+    function promptDescription() external view returns (string memory) {
+        return prompt;
+    }
 }
